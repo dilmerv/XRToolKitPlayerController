418adbe070d8314941b00b95cfa237daf344a1d2
Dilmer Valecillos <dilmervst@gmail.com>
2020-05-06 08:48:10 -0600

    Added tests and code coverage reports

---
+++ Assets/Scripts/Tests/XRPlayerControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Tests
+{
+    public class XRPlayerControllerTests
+    {
+        private const string SCENE_TO_LOAD = "XRPlayerController";
+        private const int SCENE_WAIT_TIME = 1;
+
+        private XRRig XRRig { get; set; }
+
+        [UnitySetUp]
+        public IEnumerator XRPlayerController_XRRig_Setup()
+        {
+            SceneManager.LoadScene(SCENE_TO_LOAD);
+
+            yield return new WaitForSeconds(SCENE_WAIT_TIME);
+
+            XRRig = GameObject.FindObjectOfType<XRRig>();
+        }
+
+        [UnityTest]
+        public IEnumerator XRPlayerController_XRRig_With_WaitTime_Exist()
+        {  
+            Assert.IsNotNull(XRRig);
+            yield return null;
+        }
+
+        [Test]
+        public void XRPlayerController_XRRig_XRPlayerController_Exist()
+        {
+            Assert.IsNotNull(XRRig.GetComponent<XRPlayerController>());
+        }
+    }
+}
